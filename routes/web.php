f6d28a7c97ae527a3a5fa2ff1900855e4cb0aea2
Toby <thobisaha@gmail.com>
2022-12-26 08:56:29 +0700

    membuat middleware group route

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,14 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 
-Route::get('login', [AuthController::class, 'login'])->name('login')->middleware('only_guest');
-Route::post('login', [AuthController::class, 'authenticating'])->middleware('only_guest');
-Route::get('register', [AuthController::class, 'register'])->middleware('only_guest');
+Route::middleware('only_guest')->group(function () {
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+    Route::post('login', [AuthController::class, 'authenticating']);
+    Route::get('register', [AuthController::class, 'register']);
+});
 
-Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'only_admin']);
-Route::get('profile', [UserController::class, 'profile'])->middleware(['auth', 'only_client']);
-Route::get('books', [BookController::class, 'index'])->middleware('auth');
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->middleware('only_admin');
+    Route::get('profile', [UserController::class, 'profile'])->middleware('only_client');
+    Route::get('books', [BookController::class, 'index']);
+});
