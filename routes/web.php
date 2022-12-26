24b16c6847692a4c9e6b12a36ae6652739824d03
Toby <thobisaha@gmail.com>
2022-12-26 08:24:45 +0700

    Membuat middleware onlyAdmin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +20,9 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'authenticating']);
-Route::get('/register', [AuthController::class, 'register']);
+Route::get('register', [AuthController::class, 'register']);
+
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'only_admin']);
+Route::get('profile', [UserController::class, 'profile'])->middleware('auth');
