f7705e76d6946d779f3d98f6416cd7f29c741eb0
Toby <thobisaha@gmail.com>
2022-12-29 02:29:36 +0700

    merubah active navbar categories

--- resources/views/template/tmpDashboard.blade.php
+++ resources/views/template/tmpDashboard.blade.php
@@ -37,7 +37,7 @@
                     @if (Auth::user() -> role_id ==1) 
                         <a href="dashboard" @if (request()->route()->uri == 'dashboard') class='active'@endif>Dashboard</a>
                         <a href="books" @if (request()->route()->uri == 'books') class='active'@endif>Books</a>
-                        <a href="categories" @if (request()->route()->uri == 'categories') class='active'@endif>Categories</a>
+                        <a href="categories" @if (request()->route()->uri == 'categories' || request()->route()->uri == 'category-add' || request()->route()->uri == 'category-edit/{slug}' || request()->route()->uri == 'category-deleted' || request()->route()->uri == 'category-delete/{slug}') class='active'@endif>Categories</a>
                         <a href="users" @if (request()->route()->uri == 'users') class='active'@endif>Users</a>
                         <a href="rentLogs" @if (request()->route()->uri == 'rentLogs') class='active'@endif>Rent Logs</a>
                         <a href="logout">Logout</a>
